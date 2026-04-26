f15fe7cad6f8b579b0cb5c9c5386c477c2fd1e94
Zylomart-co <gauravsharma375453@gmail.com>
2026-04-27 01:02:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-const API = "http://localhost:5000/api/products";
+const products = [
+  {
+    name: "Classic Black Shirt",
+    price: 599,
+    image: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf"
+  },
+  {
+    name: "Ribbed Crop Top",
+    price: 399,
+    image: "https://images.unsplash.com/photo-1520975922203-bf7f0c36a0b0"
+  },
+  {
+    name: "Blue Jeans",
+    price: 799,
+    image: "https://images.unsplash.com/photo-1582418702059-97ebafb35d09"
+  },
+  {
+    name: "Black Dress",
+    price: 999,
+    image: "https://images.unsplash.com/photo-1521334884684-d80222895322"
+  }
+];
 
-async function loadProducts() {
-  const res = await fetch(API);
-  const data = await res.json();
+const container = document.getElementById("products");
 
-  const container = document.getElementById("products");
-
-  data.forEach(p => {
-    container.innerHTML += `
-      <div class="card">
-        <img src="${p.image}">
-        <h3>${p.name}</h3>
-        <p>₹${p.price}</p>
-        <button onclick="addToCart()">Add to Cart</button>
-      </div>
-    `;
-  });
-}
-
-loadProducts();
-
-function addToCart() {
-  alert("Added to cart");
-}
+products.forEach(p => {
+  container.innerHTML += `
+    <div class="card">
+      <img src="${p.image}">
+      <h3>${p.name}</h3>
+      <p>₹${p.price}</p>
+      <button>Add to Cart</button>
+    </div>
+  `;
+});
