0fc600c9dad3fc82ed816aac1437913436be4b46
Zylomart-co <gauravsharma375453@gmail.com>
2026-04-26 23:57:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const API = "http://localhost:5000/api/products";
 
+async function loadProducts() {
+  const res = await fetch(API);
+  const data = await res.json();
+
+  const container = document.getElementById("products");
+
+  data.forEach(p => {
+    container.innerHTML += `
+      <div class="card">
+        <img src="${p.image}">
+        <h3>${p.name}</h3>
+        <p>₹${p.price}</p>
+        <button onclick="addToCart()">Add to Cart</button>
+      </div>
+    `;
+  });
+}
+
+loadProducts();
+
+function addToCart() {
+  alert("Added to cart");
+}
